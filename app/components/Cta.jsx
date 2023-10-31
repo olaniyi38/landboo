e207e947c8a7301179318263b277d6131a76c291
Ljr777 <benughegbe@gmail.com>
2023-10-31 05:47:03 -0700

    remove record section, structure cta and guide

---
+++ app/components/Cta.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+const Cta = () =>{
+    return(
+<div>
+    {/* content */}
+    <div>
+
+    </div>
+    {/* image */}
+    <div>
+        <Image src="/images/" width={}height={} alt="cta image"/>
+    </div>
+</div>
+    );
+}
--- app/components/Guide.jsx
+++ app/components/Guide.jsx
@@ -2,19 +2,28 @@
 const Guide = () =>{
     return (
         <div>
-            <h2>Own a property in few steps</h2>
-            <h4>Follow these easy steps to purchase any property of your choice</h4>
-            Browse our catalog
-            See 3D renderings of our buildings on completion
-
-            Choose the property you like
-
-
-            Contact our sales department
-            For more information and make a payment schedule
-
-            Get all the documents
-            Final
+            <div>
+                <h2>Own a property in few steps</h2>
+                <h4>Follow these easy steps to purchase any property of your choice</h4>
+            </div>
+            <div>
+                <div>
+                    <h3>Browse our catalog</h3>
+                    <h4>See 3D renderings of our buildings on completion</h4>
+                </div>
+                <div>
+                    <h3>Choose the property you like</h3>
+                    <h4></h4>
+                </div>
+                <div>
+                    <h3>Contact our sales department</h3>
+                    <h4>For more information and make a payment schedule</h4>
+                </div>
+                <div>
+                    <h3>Get all the documents</h3>
+                    <h4>Final</h4>
+                </div>
+            </div>
         </div>
     );
 }
