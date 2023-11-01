c0bc3123fdb0ff65acff791427d99761538e1a10
sodiq <sodiqalao38@gmail.com>
2023-11-01 07:50:17 +0100

    update

--- app/components/Cta.jsx
+++ app/components/Cta.jsx
@@ -1,16 +1,14 @@
 import Image from "next/image";
 
-const Cta = () =>{
-    return(
-<div>
-    {/* content */}
-    <div>
+const Cta = () => {
+	return (
+		<div>
+			{/* content */}
+			<div></div>
+			{/* image */}
+			<div></div>
+		</div>
+	);
+};
 
-    </div>
-    {/* image */}
-    <div>
-        <Image src="/images/" width={}height={} alt="cta image"/>
-    </div>
-</div>
-    );
-}
+export default Cta;
