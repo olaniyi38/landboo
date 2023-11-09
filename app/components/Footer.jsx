11f21926fbdd3a7cfc31d21dfbeab2016c6e7cd2
Ljr777 <benughegbe@gmail.com>
2023-11-09 02:51:28 -0800

    landboo to jrosy homes

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -110,7 +110,7 @@ const Footer = () => {
 					</div> */}
 				</div>
 				<aside className=" border-t border-t-gray-300 py-2 md:py-4 flex flex-col md:flex-row md:justify-between gap-3">
-					<p className=" text-sm">© 2023 Landboo. All rights reserved</p>
+					<p className=" text-sm">© 2023 Jrosy Homes. All rights reserved</p>
 					<div className=" flex gap-2 lg:gap-4">
 						<i className="bg-gray-800 p-1 md:p-2 rounded-full">
 							<AiOutlineInstagram className="w-5 h-5  lg:w-6 lg:h-6" />
